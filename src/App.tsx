6d92e09e298c46651dfa8e83f8ad074f48321632
webdevproproject <silvermatambo@gmail.com>
2026-09-23 02:18:42 +0200

    Wire admin preview route

--- src/App.tsx
+++ src/App.tsx
@@ -10,9 +10,10 @@ import FloatingWhatsApp from './components/FloatingWhatsApp';
 import Home from './pages/Home';
 import ContentPage from './pages/ContentPage';
 import Contact from './pages/Contact';
+import Admin from './pages/Admin';
 
 function ServiceWorkerRegistrar(){useEffect(()=>{if('serviceWorker'in navigator){window.addEventListener('load',()=>navigator.serviceWorker.register('/sw.js').catch(()=>{}))}},[]);return null}
 export default function App(){return <BrowserRouter><ServiceWorkerRegistrar/><LoadingScreen/><div className="min-h-screen flex flex-col"><Navbar/><main className="flex-1"><Routes>
 <Route path="/" element={<Home/>}/>
-<Route path="/namibia" element={<ContentPage type="namibia"/>}/><Route path="/namibia/self-drive" element={<ContentPage type="selfdrive"/>}/><Route path="/stay-in-namibia" element={<ContentPage type="stay"/>}/><Route path="/car-hire-4x4" element={<ContentPage type="car"/>}/><Route path="/journeys" element={<ContentPage type="journeys"/>}/><Route path="/seven-churches-2027" element={<ContentPage type="churches"/>}/><Route path="/travel-guides" element={<ContentPage type="guides"/>}/><Route path="/plan-book" element={<ContentPage type="plan"/>}/><Route path="/about" element={<ContentPage type="about"/>}/><Route path="/legal" element={<ContentPage type="legal"/>}/><Route path="/contact" element={<Contact/>}/><Route path="*" element={<Home/>}/>
+<Route path="/namibia" element={<ContentPage type="namibia"/>}/><Route path="/namibia/self-drive" element={<ContentPage type="selfdrive"/>}/><Route path="/stay-in-namibia" element={<ContentPage type="stay"/>}/><Route path="/car-hire-4x4" element={<ContentPage type="car"/>}/><Route path="/journeys" element={<ContentPage type="journeys"/>}/><Route path="/seven-churches-2027" element={<ContentPage type="churches"/>}/><Route path="/travel-guides" element={<ContentPage type="guides"/>}/><Route path="/plan-book" element={<ContentPage type="plan"/>}/><Route path="/about" element={<ContentPage type="about"/>}/><Route path="/legal" element={<ContentPage type="legal"/>}/><Route path="/contact" element={<Contact/>}/><Route path="/admin" element={<Admin/>}/><Route path="*" element={<Home/>}/>
 </Routes></main><Footer/></div><FloatingWhatsApp/><PWAInstall/><MobileBottomMenu/><ScrollToTop/></BrowserRouter>}
